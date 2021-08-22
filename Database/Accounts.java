e80037cbff16f62b6c31e8f0bf710bbf669f9aaa
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2021-08-21 19:16:12 -0600

    Create Accounts.java

---
+++ Database/Accounts.java
@@ -0,0 +1,26 @@
+package Database;
+
+public class Accounts {
+
+	private final double balance;
+
+	public Accounts(double balance) {
+		this.balance = balance;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void depositMoney(int amount) {
+		newBalance(getBalance() + amount);
+	}
+
+	protected void newBalance(double d) {
+	}
+
+	public void withdrawMoney(int amount) {
+		newBalance(getBalance() - amount);
+	}
+	
+}
