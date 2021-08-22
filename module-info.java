cba06c25750be03aab56269679964eb947fda6b6
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2021-08-21 19:16:39 -0600

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,10 @@
+module Project {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.base;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires java.logging;
+	opens Controllers to javafx.graphics, javafx.fxml;
+	opens Database to javafx.graphics, javafx.base, javafx.controls;
+}
