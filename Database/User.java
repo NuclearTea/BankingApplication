d965de5b51f626136bcd4b8d80e650534c8db597
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2021-08-22 21:22:28 -0600

    Update User.java

--- Database/User.java
+++ Database/User.java
@@ -1,46 +1,91 @@
 package Database;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
 public class User {
-	static String Username;
-	static String Password;
+	public UserInfo UserInfo;
+	public ArrayList<Accounts> AccountsList;
+	public ArrayList<Bill> BillList;
+
+	/**
+	 * @return the userInfo
+	 */
+	public UserInfo getUserInfo() {
+		return UserInfo;
+	}
+
+	/**
+	 * @param userInfo the userInfo to set
+	 */
+	public void setUserInfo(UserInfo userInfo) {
+		UserInfo = userInfo;
+	}
+
+	/**
+	 * @return the accountsList
+	 */
+	public ArrayList<Accounts> getAccountsList() {
+		return AccountsList;
+	}
+
+	/**
+	 * @param accountsList the accountsList to set
+	 */
+	public void setAccountsList(ArrayList<Accounts> accountsList) {
+		AccountsList = accountsList;
+	}
+
+	/**
+	 * @return the billList
+	 */
+	public ArrayList<Bill> getBillList() {
+		return BillList;
+	}
+
+	/**
+	 * @param billList the billList to set
+	 */
+	public void setBillList(ArrayList<Bill> billList) {
+		BillList = billList;
+	}
 
 	public User() {
 
 	}
 
-	public User(String username, String password) {
-		User.Username = username;
-		User.Password = password;
+	public User(UserInfo userInfo) {
+		this.UserInfo = userInfo;
 	}
 
-	/**
-	 * @return the username
-	 */
-	public static String getUsername() {
-		return Username;
+	public User(ArrayList<Bill> billList) {
+		this.BillList = billList;
 	}
 
-	/**
-	 * @param username the username to set
-	 */
-	public static void setUsername(String username) {
-		Username = username;
+	public User(UserInfo UserInfo, ArrayList<Accounts> AccountsList, ArrayList<Bill> BillList) {
+		this.UserInfo = UserInfo;
+		this.AccountsList = AccountsList;
+		this.BillList = BillList;
+
 	}
 
-	/**
-	 * @return the password
-	 */
-	public static String getPassword() {
-		return Password;
+	public void SavingUser(String FileName) throws FileNotFoundException, UnsupportedEncodingException {
+		User.this.getUserInfo();
+		String Username = Database.UserInfo.getUsername();
+		String Password = Database.UserInfo.getPassword();
+
+		PrintWriter writer = new PrintWriter(FileName, "UTF-8");
+		writer.println(Username);
+		writer.println(Password);
+		for (Accounts accounts : User.this.getAccountsList()) {
+			writer.println(accounts);
+		}
+		for (Bill bills : User.this.getBillList()) {
+			writer.println(bills);
+		}
+		writer.close();
 	}
 
-	/**
-	 * @param password the password to set
-	 */
-	public static void setPassword(String password) {
-		Password = password;
-	}
-
-	
-	
 }
